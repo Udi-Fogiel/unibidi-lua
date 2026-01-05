807d729e00126420f7c20d000cd734101c37ea63
udifogiel <udifogiel@proton.me>
2026-01-05 04:20:08 +0200

    ammend e906145

--- unibidi-lua.lua
+++ unibidi-lua.lua
@@ -475,9 +475,9 @@ local function get_baselevel(head,list,size,direction)
         if direction == lefttoright_code or direction == righttoleft_code then
             return direction, true
         end
-    end
     elseif direction == lefttoright_code or direction == righttoleft_code then
         return direction, true
+    end
     -- P2, P3
     for i=1,size do
         local entry     = list[i]
